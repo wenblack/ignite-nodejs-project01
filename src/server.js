c390902f0e6944e5471e9a50b97c078a4b4630b3
Wen Barbosa <wenblack012@gmail.com>
2023-10-22 23:04:45 -0300

    Created headers and initial data from server/api

--- src/server.js
+++ src/server.js
@@ -1,14 +1,23 @@
 //use 'node:' to import internal modules
 import http from 'node:http'
 
+const users = []
+
 const server = http.createServer((req, res) => {
   const { method, url } = req
 
   if (method === 'GET' && url === '/users') {
-    return res.end('User List')
+    return res
+      .setHeader('Content-type', 'application/json')
+      .end(JSON.stringify(users))
   }
 
   if (method === 'POST' && url === '/users') {
+    users.push({
+      id: 1,
+      name: 'John Doe',
+      email: 'johndoe@example.com'
+    })
     return res.end('User Create')
   }
   return res.end('Hello World')
