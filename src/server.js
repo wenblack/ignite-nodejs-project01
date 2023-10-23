5318aebc835a5f01e5bf25464470ecf50e93a30a
Wen Barbosa <wenblack012@gmail.com>
2023-10-22 22:51:22 -0300

    Created initial routes for server/app

--- src/server.js
+++ src/server.js
@@ -2,6 +2,15 @@
 import http from 'node:http'
 
 const server = http.createServer((req, res) => {
+  const { method, url } = req
+
+  if (method === 'GET' && url === '/users') {
+    return res.end('User List')
+  }
+
+  if (method === 'POST' && url === '/users') {
+    return res.end('User Create')
+  }
   return res.end('Hello World')
 })
 
