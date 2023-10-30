035f416d3fbd54a94587753083a7860ba4f999f0
Wen Barbosa <wenblack012@gmail.com>
2023-10-30 16:04:10 -0300

    Created 1st Middleware

---
+++ src/middlewares/json.js
@@ -0,0 +1,17 @@
+export async function json(req, res) {
+  const buffers = []
+  //async function to set/get data for Array
+  for await (const chunk of req) {
+    buffers.push(chunk)
+  }
+
+  //try function to convert data to JSON (for response)
+  try {
+    req.body = JSON.parse(Buffer.concat(buffers).toString())
+    console.log(req.body)
+  } catch {
+    req.body = null
+  }
+
+  res.setHeader('Content-type', 'application/json')
+}
--- src/server.js
+++ src/server.js
@@ -1,24 +1,14 @@
 //use 'node:' to import internal modules
 import http from 'node:http'
+import { json } from './middlewares/json.js'
 
 const users = []
 
 const server = http.createServer(async (req, res) => {
   const { method, url } = req
-  //Array of string data
-  const buffers = []
-  //async function to set/get data for Array
-  for await (const chunk of req) {
-    buffers.push(chunk)
-  }
 
-  //try function to convert data to JSON (for response)
-  try {
-    req.body = JSON.parse(Buffer.concat(buffers).toString())
-    console.log(req.body)
-  } catch {
-    req.body = null
-  }
+  await json(req, res)
+
 
   if (method === 'GET' && url === '/users') {
     return res
