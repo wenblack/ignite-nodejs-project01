f9f1c340613645ac0cf10730d91397e7e9739e0b
Wen Barbosa <wenblack012@gmail.com>
2023-10-30 17:20:26 -0300

    Import Node FileSystem(fs) module to read and write database file

---
+++ src/database.js
@@ -0,0 +1,41 @@
+//Imported fs to read and write files
+import fs from 'node:fs/promises'
+
+//Path destination of database file
+const databasePath = new URL('../db.json', import.meta.url)
+
+export class Database {
+  #database = {}
+
+  constructor() {
+    fs.readFile(databasePath, 'utf8')
+      .then(data => {
+        this.#database = JSON.parse(data)
+      })
+      .catch(() => {
+        this.#persist()
+      })
+  }
+
+  #persist() {
+    fs.writeFile(databasePath, JSON.stringify(this.#database))
+  }
+
+  select(table) {
+    const data = this.#database[table] ?? []
+
+    return data
+  }
+
+  insert(table, data) {
+    if (Array.isArray(this.#database[table])) {
+      this.#database[table].push(data)
+    } else {
+      this.#database[table] = [data]
+    }
+
+    this.#persist()
+
+    return data
+  }
+}
--- src/server.js
+++ src/server.js
@@ -1,8 +1,9 @@
 //use 'node:' to import internal modules
 import http from 'node:http'
 import { json } from './middlewares/json.js'
+import { Database } from './database.js'
 
-const users = []
+const database = new Database()
 
 const server = http.createServer(async (req, res) => {
   const { method, url } = req
@@ -11,19 +12,21 @@ const server = http.createServer(async (req, res) => {
 
 
   if (method === 'GET' && url === '/users') {
-    return res
-      .setHeader('Content-type', 'application/json')
-      .end(JSON.stringify(users))
+    const users = database.select('users')
+
+    return res.end(JSON.stringify(users))
   }
 
   if (method === 'POST' && url === '/users') {
     const { name, email } = req.body
 
-    users.push({
+    const user = {
       id: 1,
       name,
       email,
-    })
+    }
+    database.insert('users', user)
+
     return res
       .writeHead(201)
       .end()
